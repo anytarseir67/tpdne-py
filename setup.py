71317a81d1714a0e2b3b632d7d634f33f3524b83
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-10-27 15:25:27 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
 setup(name='tpdne-py',
-    version='0.0.1',
+    version='0.0.2',
     description='a very simple wrapper around requests/aiohttp for thispersondoesnotexist.com',
     long_description=README,
     long_description_content_type="text/markdown",
